3b8494b3ab03c203e3dbb26aba6f3c55d4952c66
LAPTOP-R18K9KEB\walsh <walshg33@gmail.com>
2020-09-13 22:12:29 -0700

    Getting rid of extra default code that I forgot to delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -16,9 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
       </header>
-      <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
