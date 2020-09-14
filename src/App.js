4017c9cedd8af836d1b9fcb238e2e6279dd74e68
LAPTOP-R18K9KEB\walsh <walshg33@gmail.com>
2020-09-14 00:09:59 -0700

    Comment out previous code, create helloWorld const

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    // next line added
+    const helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        <header className="App-header">
+        {/* next line added */}
+        <h2>{helloWorld}</h2>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
