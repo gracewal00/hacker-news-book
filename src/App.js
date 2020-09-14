94393db2d21113c2e4a3e91a8cc13c24e9f9889e
LAPTOP-R18K9KEB\walsh <walshg33@gmail.com>
2020-09-13 22:07:09 -0700

    Use the book's code instead of the default create-react-app code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
       </header>
-    </div>
-  );
+      <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      </div>
+    );
+  }
 }
 
 export default App;
