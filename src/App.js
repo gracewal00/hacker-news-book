b879b1d01930395675a929f875c224226fc9cb37
LAPTOP-R18K9KEB\walsh <walshg33@gmail.com>
2021-02-18 17:12:12 -0800

    Prevent calling setState on unmounted component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
+  _isMounted = false;
 
   constructor(props) {
     super(props);
@@ -58,14 +59,20 @@ class App extends Component {
 
   fetchSearchTopStories(searchTerm, page = 0) {
     axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(result => this.setSearchTopStories(result.data))
-      .catch(error => this.setState({ error }));
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
   }
 
   componentDidMount() {
+    this._isMounted = true;
+
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
+  }
+
+  ComponentWillUnmount() {
+    this._isMounted = false;
   }
 
   onSearchChange(event) {
